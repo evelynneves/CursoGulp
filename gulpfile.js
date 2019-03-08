70878a3b7546d0be9232c5f8cd5cbcdd7eeb684a
Evelyn Neves Barreto <enevesba@everis.com>
2019-03-08 13:35:34 -0300

    aula 03

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require("gulp");
 imagemin = require("gulp-imagemin");
 clean = require("gulp-clean");
+concat = require('gulp-concat');
+htmlReplace = require('gulp-html-replace');
+uglify = require('gulp-uglify');
+usemin = require('gulp-usemin');
+cssmin = require('gulp-cssmin');
+
+gulp.task('default', ['copy'], function() {
+  gulp.start('build-img', 'usemin')
+});
 
 gulp.task("copy", ["clean"], function() {
   return gulp.src("src/**/*").pipe(gulp.dest("dist"));
@@ -10,9 +19,18 @@ gulp.task("clean", function() {
   return gulp.src("dist").pipe(clean());
 });
 
-gulp.task("build-img", ["copy"], function() {
+gulp.task("build-img", function() {
   gulp
     .src("dist/img/**/*")
     .pipe(imagemin())
     .pipe(gulp.dest("dist/img"));
 });
+
+gulp.task('usemin', function() {
+  gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      'js' : [uglify],
+      'css' : [cssmin]
+    }))
+    .pipe(gulp.dest('dist'))
+})
